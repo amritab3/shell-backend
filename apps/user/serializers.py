ff86e767ca31274043d50ffd1db9da7d7da52fbf
Shelly <shellybhattarai@gmail.com>
2024-02-01 15:04:37 +0545

    add views and serializers for user register

---
+++ apps/user/serializers.py
@@ -0,0 +1,15 @@
+from .models import User
+from rest_framework import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["email", "password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User(email=validated_data["email"])
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import UserSerializer
+
+# from .permissions import UserPermission
+from .models import User
+
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all().order_by("-date_joined")
