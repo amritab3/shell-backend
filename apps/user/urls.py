b3fac223a0deeadc13dc1531d38fe0888df411bd
Shelly <shellybhattarai@gmail.com>
2024-02-01 15:21:44 +0545

    list user api

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
 
-from .views import CreateUserView
+from .views import CreateUserView, ListUserView
 
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="token_obtain_pair"),
+    path("register/", CreateUserView.as_view(), name="create_user"),
     path(
         "login/",
         jwt_views.TokenObtainPairView.as_view(),
@@ -15,4 +15,5 @@ urlpatterns = [
         jwt_views.TokenRefreshView.as_view(),
         name="token_refresh",
     ),
+    path("list/", ListUserView.as_view(), name="list_user"),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,4 +1,3 @@
-from rest_framework.viewsets import ModelViewSet
 from rest_framework import generics
 
 from .serializers import UserSerializer
@@ -10,3 +9,9 @@ from .models import User
 class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
     queryset = User.objects.all().order_by("-date_joined")
+
+
+class ListUserView(generics.ListAPIView):
+    model = User
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
