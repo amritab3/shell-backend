f121970429cb5a732adac21bbc002e6067727cd3
Shelly <shellybhattarai@gmail.com>
2024-02-01 15:03:34 +0545

    add custom user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_("Email is required."))
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(email, password, **extra_fields)
+
 
 # Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(verbose_name="email", max_length=60, unique=True)
+    date_joined = models.DateTimeField(
+        verbose_name="date joined", auto_now_add=True
+    )
+    last_login = models.DateTimeField(verbose_name="last login", auto_now=True)
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=30)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
