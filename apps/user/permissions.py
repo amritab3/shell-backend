d8ad89564c7cb4d209df10c349315839e4526ba8
Shelly <shellybhattarai@gmail.com>
2024-04-27 23:19:55 +0545

    remove print

--- apps/user/permissions.py
+++ apps/user/permissions.py
@@ -5,8 +5,5 @@ class IsOwner(permissions.BasePermission):
     message = "You do not have permission to perform this action."
 
     def has_object_permission(self, request, view, obj):
-        print("Second Print")
-        print("obj", obj)
-        print("req", request.user)
         # Check if the user is the owner of the object
         return obj == request.user
