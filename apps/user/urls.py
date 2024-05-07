910828e7bd010d417bb8f3d7e0c3fddb7b0fd2b4
Shelly <shellybhattarai@gmail.com>
2024-05-07 19:23:16 +0545

    fix url

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -15,12 +15,12 @@ router1 = DefaultRouter()
 router2 = DefaultRouter()
 
 router1.register(r"user", UserAdminViewSet)
-router2.register("cart", CartViewSet)
+router2.register("", CartViewSet, basename="user_cart")
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create_user"),
-    path("admin", include(router1.urls)),
-    path("<str:userId>/", include(router2.urls)),
+    path("admin/", include(router1.urls)),
+    path("<str:userId>/cart/", include(router2.urls)),
     path("", ListUserView.as_view(), name="list_user"),
     path("<str:pk>/", RetrieveUpdateDeleteUser.as_view(), name="user-detail"),
 ]
